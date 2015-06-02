4563196b7d54cfc49cd47a30f25923793410498f
jwgibbs <mikewood@jlab.org>
2015-06-01 20:25:44 -0400

    Updating macro to 2D histogram of e- z vertices

--- clas6/DMS/eg2/omega/macros/Zvertex/eg2a_Zvert.C
+++ clas6/DMS/eg2/omega/macros/Zvertex/eg2a_Zvert.C
@@ -16,7 +16,6 @@ const Int_t NSECTORS= 6;
 Int_t lcol[10] = {1,2,4,6,7,8,9,10,14,15};
 Int_t mkr[10] = {20,21,22,24,23,25,26,27,28,29};
 char *fSame[10] = {"","same","same","same","same","same","same","same","same","same"};
-char *Plabel[3] = {"#pi^{+}","#pi^{-}","#pi^{+}#pi^{-}"};
 
 char hname[50];
 char htitle[500];
@@ -39,39 +38,51 @@ Float_t yoff = 1.5;
 //                  suffix = string to append to end of image file name
 //                  target = target name
 //
-void PlotZvert(char *fAna="Ana.root", char *suffix, char *target)
+void PlotZvert(char *fAna="Ana.root", char *target, int sector)
 {
-	
-	// Canvas to plot histogram
-	TCanvas *c1 = new TCanvas("c1","c1",0,0,600,600);
-	c1->SetBorderMode(1);  //Bordermode (-1=down, 0 = no border, 1=up)
-	c1->SetBorderSize(5); 
-	gStyle->SetOptStat(0);
-	c1->SetFillStyle(4000);
-	
-	// data files contain the trees
-	printf("Analyzing file %s\n",fAna);  
-	TFile *fm = new TFile(fAna,"READ");
-	
-	c1->cd();
-	gPad->SetLeftMargin(Lmar);
-	gPad->SetRightMargin(Rmar);
-	gPad->SetFillColor(0);
+    char strname[50];
     
-	TH1F *hZvert = (TH1F*)fm->Get("elecZVert");
-	hZvert->SetTitle(target);
-	hZvert->SetXTitle("e^{-} Z Vertex (cm)");
-	hZvert->GetXaxis()->CenterTitle();
-	hZvert->SetYTitle("Counts");
-	hZvert->GetYaxis()->CenterTitle();
-	hZvert->GetYaxis()->SetTitleOffset(yoff);
-	hZvert->SetLineWidth(2);
-	hZvert->Draw();
+    if(sector>0 && sector<=6){
+        // Canvas to plot histogram
+        TCanvas *c1 = new TCanvas("c1","c1",0,0,600,600);
+        c1->SetBorderMode(1);  //Bordermode (-1=down, 0 = no border, 1=up)
+        c1->SetBorderSize(5);
+        gStyle->SetOptStat(0);
+        c1->SetFillStyle(4000);
+	
+        // data files contain the trees
+        printf("Analyzing file %s\n",fAna);
+        TFile *fm = new TFile(fAna,"READ");
+        TDirectory *tmp = fm->GetDirectory("Kinematics");
 
-	sprintf(OutCan,"PlotZvert_%s.gif",suffix);
-	c1->Print(OutCan);
-	sprintf(OutCan,"PlotZvert_%s.eps",suffix);
-	c1->Print(OutCan);
+        c1->cd();
+        gPad->SetLeftMargin(Lmar);
+        gPad->SetRightMargin(Rmar);
+        gPad->SetFillColor(0);
+    
+        sprintf(hname,"elecZVertSector");
+        TH2F *hZvert = (TH2F*)tmp->Get(hname);
+
+        sprintf(strname,"%s_px_%i",hname,sector);
+        hZvert_proj = (TH1D*)hZvert->ProjectionX(strname,sector,sector,"");
+    
+        sprintf(title,"%s, Sector %i",target,sector);
+        hZvert_proj->SetTitle(title);
+        hZvert_proj->SetXTitle("e^{-} Z Vertex (cm)");
+        hZvert_proj->GetXaxis()->CenterTitle();
+        hZvert_proj->SetYTitle("Counts");
+        hZvert_proj->GetYaxis()->CenterTitle();
+        hZvert_proj->GetYaxis()->SetTitleOffset(yoff);
+        hZvert_proj->SetLineWidth(2);
+        hZvert_proj->Draw();
+
+        sprintf(OutCan,"PlotZvert_%s_S%i.gif",target,sector);
+        c1->Print(OutCan);
+        sprintf(OutCan,"PlotZvert_%s_S%i.eps",target,sector);
+        c1->Print(OutCan);
+    }else{
+        cout<<"Wrong sector number "<<sector<<endl;
+    }
 }
 
 //
@@ -81,10 +92,11 @@ void PlotZvert(char *fAna="Ana.root", char *suffix, char *target)
 //                  suffix = string to append to end of image file name
 //                  target = target name
 //
-void OverlayZvertBySector(char *fAna="Ana.root", char *suffix, char *target)
+void OverlayZvertBySector(char *fAna="Ana.root", char *target)
 {
 	Int_t i;
 	char legLabel[50];
+    char strname[50];
     
 	// Canvas to plot histogram
 	TCanvas *c1 = new TCanvas("c1","c1",0,0,600,600);
@@ -96,19 +108,23 @@ void OverlayZvertBySector(char *fAna="Ana.root", char *suffix, char *target)
 	// data files contain the trees
 	printf("Analyzing file %s\n",fAna);
 	TFile *fm = new TFile(fAna,"READ");
-	
-	c1->cd();
+    TDirectory *tmp = fm->GetDirectory("Kinematics");
+
+    c1->cd();
 	gPad->SetLeftMargin(Lmar);
 	gPad->SetRightMargin(Rmar);
 	gPad->SetFillColor(0);
+ 
+    sprintf(hname,"elecZVertSector");
+    TH2F *hZvert = (TH2F*)tmp->Get(hname);
     
-	TH1F *hZSec[NSECTORS];
+	TH1D *hZSec[NSECTORS];
     
 	TLegend *leg = new TLegend(0.60,0.50,1.0,0.85); //declare Legend and give its location
     
     	for(i=0; i<NSECTORS ; i++){
-        	sprintf(hname,"elecZVertSector%i",i+1);
-        	hZSec[i] = (TH1F*)fm->Get(hname);
+            sprintf(strname,"%s_px_%i",hname,i+1);
+            hZSec[i] = (TH1D*)hZvert->ProjectionX(strname,i+1,i+1,"");
         	hZSec[i]->SetTitle(0);
         	hZSec[i]->SetXTitle("e^{-} Z Vertex (cm)");
         	hZSec[i]->GetXaxis()->CenterTitle();
@@ -127,9 +143,9 @@ void OverlayZvertBySector(char *fAna="Ana.root", char *suffix, char *target)
     	leg->SetHeader(target);
     	leg->Draw();
     
-	sprintf(OutCan,"OverlayZvertBySector_%s.gif",suffix);
+	sprintf(OutCan,"OverlayZvertBySector_%s.gif",target);
 	c1->Print(OutCan);
-	sprintf(OutCan,"OverlayZvertBySector_%s.eps",suffix);
+	sprintf(OutCan,"OverlayZvertBySector_%s.eps",target);
 	c1->Print(OutCan);
 }
 
