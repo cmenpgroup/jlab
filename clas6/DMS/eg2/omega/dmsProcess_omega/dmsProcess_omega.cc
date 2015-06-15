9e9b9460ee9c3c6a80bdb9a261a4a4238ac1cdf8
jwgibbs <mikewood@jlab.org>
2015-06-15 09:03:54 -0400

    Testing the Print() in the TVector3 method

--- clas6/DMS/eg2/omega/dmsProcess_omega/dmsProcess_omega.cc
+++ clas6/DMS/eg2/omega/dmsProcess_omega/dmsProcess_omega.cc
@@ -266,7 +266,7 @@ int process (string inFile, int MaxEvents, int dEvents, int targMass) {
         elec_vert_corr = myVertCorr.Get_Particle_Vertex_Corrected();
 
         cout<<"Testing "<<endl;
-        elec.Vect().Print()
+        elec.Vect().Print();
         elec.Print();
         
         //_________________________________
